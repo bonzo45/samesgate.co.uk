fd32906ed4533a3d1eb5f971fd3f4cf305a6110c
bonzo45 <sam.esgate11@imperial.ac.uk>
2014-05-17 20:25:40 +0100

    Experimenting with Leaflet.js

---
+++ maps/icons.js
@@ -0,0 +1,21 @@
+var redIcon = L.icon({
+    iconUrl: 'leaflet/images/marker-icon-red.png',
+    iconRetinaUrl: 'leaflet/images/marker-icon-red-2x.png',
+    iconSize: [25, 41],
+    iconAnchor:  [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'leaflet/images/marker-shadow.png',
+    shadowSize:  [41, 41],
+    shadowAnchor:  [12, 41]
+});
+
+var homeIcon = L.icon({
+    iconUrl: 'leaflet/images/marker-home.png',
+    iconRetinaUrl: 'leaflet/images/marker-home-2x.png',
+    iconSize: [25, 41],
+    iconAnchor:  [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'leaflet/images/marker-shadow.png',
+    shadowSize:  [41, 41],
+    shadowAnchor:  [12, 41]
+});
---
+++ maps/places.js
@@ -0,0 +1,140 @@
+var home = {
+  "name": "Home",
+  "latLng": L.latLng(52.187421, 0.157195)
+};
+
+var shelford = {
+  "name": "Shelford",
+  "latLng": L.latLng(52.15381199999999, 0.14215630000001056)
+};
+
+var london = {
+  "name": "London",
+  "latLng": L.latLng(51.50, -0.12)
+};
+
+var brussels = {
+  "name": "Brussels",
+  "latLng": L.latLng(50.85, 4.35)
+};
+
+var bruges = {
+  "name": "Bruges",
+  "latLng": L.latLng(51.20, 3.22)
+};
+
+var amsterdam = {
+  "name": "Amsterdam",
+  "latLng": L.latLng(52.37, 4.89)
+};
+
+var berlin = {
+  "name": "Berlin",
+  "latLng": L.latLng(52.52, 13.40)
+};
+
+var prague = {
+  "name": "Prague",
+  "latLng": L.latLng(50.07, 14.43)
+};
+
+var vienna = {
+  "name": "Vienna",
+  "latLng": L.latLng(48.20, 16.37)
+};
+
+var budapest = {
+  "name": "Budapest",
+  "latLng": L.latLng(47.49, 19.04)
+};
+
+var venice = {
+  "name": "Venice",
+  "latLng": L.latLng(45.43, 12.32)
+};
+
+var milan = {
+  "name": "Milan",
+  "latLng": L.latLng(45.46, 9.18)
+};
+
+var interlaken = {
+  "name": "Interlaken",
+  "latLng": L.latLng(46.68, 7.86)
+};
+
+var paris = {
+  "name": "Paris",
+  "latLng": L.latLng(48.85, 2.35)
+};
+
+var nice = {
+  "name": "Nice",
+  "latLng": L.latLng(43.71, 7.26)
+};
+
+var monaco = {
+  "name": "Monaco",
+  "latLng": L.latLng(43.73, 7.42)
+};
+
+var brecon = {
+  "name": "Brecon Beacons",
+  "latLng": L.latLng(51.89, -3.40)
+};
+
+var kent = {
+  "name": "Kent",
+  "latLng": L.latLng(51.26, 0.84)
+};
+
+var hunstanton = {
+  "name": "Hunstanton",
+  "latLng": L.latLng(52.93, 0.49)
+};
+
+var thetford = {
+  "name": "Thetford",
+  "latLng": L.latLng(52.47, 0.67)
+};
+
+var usk = {
+  "name": "Usk",
+  "latLng": L.latLng(51.70, -2.90)
+};
+
+var cardiff = {
+  "name": "Cardiff",
+  "latLng": L.latLng(51.48, -3.17)
+};
+
+var bolton = {
+  "name": "Bolton",
+  "latLng": L.latLng(53.58, -2.42)
+};
+
+var glasgow = {
+  "name": "Glasgow",
+  "latLng": L.latLng(55.86, -4.25)
+};
+
+var whitby = {
+  "name": "Whitby",
+  "latLng": L.latLng(54.48, -0.61)
+};
+
+var scarborough = {
+  "name": "Scarborough",
+  "latLng": L.latLng(54.28, -0.39)
+};
+
+var kendal = {
+  "name": "Kendal",
+  "latLng": L.latLng(54.32, -2.74)
+};
+
+var bo = [kendal, whitby, scarborough]
+var family = [shelford, brecon, kent, hunstanton, thetford, usk, cardiff, bolton, glasgow]
+var interrail = [london, brussels, bruges, amsterdam, berlin, prague, vienna, budapest, venice, milan, interlaken, paris];
+var interrailRoute = [home, london, brussels, bruges, brussels, amsterdam, berlin, prague, vienna, budapest, vienna, venice, milan, interlaken, paris, brussels, london, home];
+var nice11 = [nice, monaco];
