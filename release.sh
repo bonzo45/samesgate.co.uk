99c37e8c929e6cbb92358cc40e07c63ed4b8cd1b
Sam Esgate <sam.esgate11@imperial.ac.uk>
2016-09-14 13:10:21 +0100

    Updated release script to remove release script.

--- release.sh
+++ release.sh
@@ -4,11 +4,16 @@ clear
 mkdir -p release
 cp -r -u * release/
 cd release
+
 find . -name "*.sass" -type f -delete
 find . -name "*._sass" -type f -delete
 find . -name "*.psd" -type f -delete
 find . -name "*.ai" -type f -delete
 
-rm -r image/design
+rm -r design/
 rm -r maps/
+rm -r release/
+
 rm README.md
+rm bower.json
+rm release.sh
