21155b1c6ee5ccf3e9e48d858423772d90cb9072
bonzo45 <sam.esgate11@imperial.ac.uk>
2016-09-15 18:09:35 +0100

    Further release script improvements.

--- release.sh
+++ release.sh
@@ -9,11 +9,13 @@ find . -name "*.sass" -type f -delete
 find . -name "*._sass" -type f -delete
 find . -name "*.psd" -type f -delete
 find . -name "*.ai" -type f -delete
+find . -name "*.db" -type f -delete
 
 rm -r design/
 rm -r maps/
 rm -r release/
 rm -r bower_components/
+rm -r css/.sass_cache
 
 rm README.md
 rm bower.json
